d07aff9f1a6cbdb463c655d88e718d356372539f
Endika Aguirre <2012mineral@gmail.com>
2026-03-21 16:18:40 +0100

    delete files

--- create_dirs.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-['src', 'include', 'shaders', 'libs/glad/include/glad', 'libs/glad/src'].forEach(d => fs.mkdirSync('c:/Users/2012m/Desktop/SIMULATOR/' + d, { recursive: true }));
-console.log('done');
